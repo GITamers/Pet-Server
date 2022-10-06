664e2a38f8d61091bd6690b5bdd7df2d528df448
devxb <devxb@xb-apple.local>
2022-10-06 16:27:30 +0900

    초기세팅

---
+++ petserver/src/main/java/net/gitpet/petserver/PetserverApplication.java
@@ -0,0 +1,13 @@
+package net.gitpet.petserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetserverApplication.class, args);
+	}
+
+}
---
+++ petserver/src/test/java/net/gitpet/petserver/PetserverApplicationTests.java
@@ -0,0 +1,13 @@
+package net.gitpet.petserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
