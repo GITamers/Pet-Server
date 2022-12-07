4478f93c39558c60b3a347c14724f0db6f3f08f9
devxb <devxb@naver.com>
2022-12-07 19:02:29 +0900

    feature : add trade-transaction, crud service

--- src/main/java/net/gitpet/petserver/service/exp/contribution/github/GithubSecret.java
+++ src/main/java/net/gitpet/petserver/service/exp/contribution/github/GithubSecret.java
@@ -2,7 +2,7 @@ package net.gitpet.petserver.service.exp.contribution.github;
 
 enum GithubSecret {
 
-    TOKEN("bearer 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");
+    TOKEN("");
 
     final String VALUE;
 
